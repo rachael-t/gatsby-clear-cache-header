879de00e24375a49bef960d2a00eb691edf7a588
Rachael Thomas <rachaelthomas@rachaels-mbp.lan>
2022-09-23 19:23:03 -0600

    Add images and trailinSlash

--- src/pages/page-2.js
+++ src/pages/page-2.js
@@ -3,12 +3,15 @@ import { Link } from "gatsby"
 
 import Layout from "../components/layout"
 import Seo from "../components/seo"
+import * as styles from "../components/index.module.css"
 
 const SecondPage = () => (
   <Layout>
-    <h1>Hi from the second page</h1>
-    <p>Welcome to page 2</p>
-    <Link to="/">Go back to the homepage</Link>
+    <div className={styles.backgroundImage}>
+      <h1>Hi from the second page</h1>
+      <p>Welcome to page 2</p>
+      <Link to="/">Go back to the homepage</Link>
+    </div>
   </Layout>
 )
 
